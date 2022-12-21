0c1925b7ec13df6b32388390730a609beb68a2be
Sheryl H <sheryl_hu0806@hotmail.com>
2022-12-21 20:31:42 +0000

    Saved progress on ForLoop 2022-12-21 20:31:42 +0000

--- ForLoop/src/main/java/ForLoop.java
+++ ForLoop/src/main/java/ForLoop.java
@@ -43,7 +43,11 @@ public class ForLoop {
         String buildingString = "";
 
 //        code here
-
+        for (int i = start; i < end; i++) {
+            buildingString = buildingString + i + " "; 
+        }
+        
         return buildingString;
     }
+
 }
