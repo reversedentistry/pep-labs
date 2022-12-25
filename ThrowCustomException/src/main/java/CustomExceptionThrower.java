baec32451e4c296771a7835f6ce143fbdbec48bf
Sheryl H <sheryl_hu0806@hotmail.com>
2022-12-25 03:46:48 +0000

    Saved progress on ThrowCustomException 2022-12-25 03:46:48 +0000

--- ThrowCustomException/src/main/java/CustomExceptionThrower.java
+++ ThrowCustomException/src/main/java/CustomExceptionThrower.java
@@ -8,8 +8,13 @@ public class CustomExceptionThrower {
      *
      * I recommend looking at the test case for a good example of try/catch block usage, as well.
      */
-    public void throwCustomException(){
-
+    public void throwCustomException() throws CustomException{
+        int age = 17; 
+        if (age < 18) {
+            throw new CustomException("You must be 18 or over to vote.");
+        } else {
+            System.out.println("All good! You may now vote."); 
+        }
     }
 
 }
