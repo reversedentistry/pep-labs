17cde28b6ae1e58d19eb79ebc40ccb0e7d021d1e
Sheryl H <sheryl_hu0806@hotmail.com>
2022-12-30 02:47:09 +0000

    Saved progress on BankAccount 2022-12-30 02:47:09 +0000

--- BankAccount/src/main/java/BankService.java
+++ BankAccount/src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        balance += amount; 
     }
 
     /**
@@ -32,7 +32,11 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if (balance - amount < 0) {
+            return; 
+        } else {
+            balance -= amount; 
+        }
     }
 
     /**
