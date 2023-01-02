f921d9285dffbcd77aeb342714076cb8eb7c7e22
Sheryl H <sheryl_hu0806@hotmail.com>
2023-01-02 21:56:03 +0000

    Saved progress on ArraysAreEqual 2023-01-02 21:56:03 +0000

--- ArraysAreEqual/src/main/java/ArraysAreEqual.java
+++ ArraysAreEqual/src/main/java/ArraysAreEqual.java
@@ -15,6 +15,16 @@ public class ArraysAreEqual {
      * @return true if the values of a are equal to the values of b.
      */
     public boolean equal(int[] a, int[] b){
-        return false;
+        boolean sameElement = false; 
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == b[i]) {
+                sameElement = true; 
+            } else {
+                return false; 
+            }
+        }
+        
+        return true;
     }
 }
