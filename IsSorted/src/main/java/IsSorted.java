559452024b3f0ce8cc2d852124f93d1ab4591a34
Sheryl H <sheryl_hu0806@hotmail.com>
2023-01-09 20:48:45 +0000

    Saved progress on IsSorted 2023-01-09 20:48:45 +0000

--- IsSorted/src/main/java/IsSorted.java
+++ IsSorted/src/main/java/IsSorted.java
@@ -9,16 +9,33 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        boolean sorted = true; 
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > arr[i++]) {
-                sorted = false;
+        
+        // for (int i = 0; i < arr.length-1; i++) {
+        //     if (arr[i] > arr[i++]) {
+        //         return false; 
              
-            } 
-        }
+        //     }; 
+        // };
 
-        return sorted; 
+        // return true;
+        if (arr[0] > arr[1]) {
+            // descending order
+            for (int i = 1; i < arr.length; i++) {
+               if (arr[i - 1] < arr[i]) {
+                  return false;
+               }
+            }
+         } else {
+            // ascending order
+            for (int i = 1; i < arr.length; i++) {
+               if (arr[i - 1] > arr[i]) {
+                  return false;
+               }
+            }
+         }
+   
+         return true;
+        
         
     }
 }
