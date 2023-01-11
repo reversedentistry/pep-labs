4c2b6ae452e1092a76158b237da8c88c24ad8fb6
Sheryl H <sheryl_hu0806@hotmail.com>
2023-01-11 04:38:45 +0000

    Saved progress on ForeignKey 2023-01-11 04:38:45 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,3 +1,4 @@
-CREATE TABLE post (id SERIAL PRIMARY KEY, post varchar(255), user_fk int, FOREIGN KEY (user_fk) REFERENCES "User"(id)); 
+CREATE TABLE post (id SERIAL PRIMARY KEY, post varchar(255), user_fk int REFERENCES "User"(id)); 
 
+-- CREATE TABLE post (id SERIAL PRIMARY KEY, post varchar(255), user_fk int, FOREIGN KEY (user_fk) REFERENCES "User"(id)); 
 -- CREATE TABLE "User" (id SERIAL PRIMARY KEY, firstname varchar(100), lastname varchar(100)); 
