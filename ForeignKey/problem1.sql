77476365856af567243b88f41a76f806b1cfb838
Sheryl H <sheryl_hu0806@hotmail.com>
2023-01-11 19:00:17 +0000

    Saved progress on InnerJoin 2023-01-11 19:00:17 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,4 +1,3 @@
-CREATE TABLE post (id SERIAL PRIMARY KEY, post varchar(255), user_fk int REFERENCES "User"(id)); 
+CREATE TABLE post (id SERIAL PRIMARY KEY, post VARCHAR(255), user_fk INT REFERENCES "User"(id)); 
 
--- CREATE TABLE post (id SERIAL PRIMARY KEY, post varchar(255), user_fk int, FOREIGN KEY (user_fk) REFERENCES "User"(id)); 
--- CREATE TABLE "User" (id SERIAL PRIMARY KEY, firstname varchar(100), lastname varchar(100)); 
+
